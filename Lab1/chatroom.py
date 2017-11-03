d790ab7a6b599dec33bb842eb1ba0d3ad29cd457
Conor Maguire <conormag94@gmail.com>
2017-11-03 19:00:39 +0000

    Add client status to retain join_ids

--- Lab1/chatroom.py
+++ Lab1/chatroom.py
@@ -8,26 +8,45 @@ class ChatRoom(object):
         self.id = id
         self.next_client_id = 1
         self.connected_clients = [
-            {"nickname": "Server", "sock": server_sock, "join_id": 0}
+            {
+                "nickname": "Server", 
+                "sock": server_sock, 
+                "join_id": 0, 
+                "status": "online"
+            }
         ]
 
     def __repr__(self):
         return "{0} ({1} online)".format(self.name, len(self.connected_clients))
 
+    def find_client_by_name(self, client_nickname):
+        for client in self.connected_clients:
+            if client["nickname"] == client_nickname:
+                return client
+        return None
+
     def add_client(self, client_sock, client_nickname, broadcast_addition=False):
         try:
-            new_client = {
-                "nickname": client_nickname, 
-                "sock": client_sock,
-                "join_id": self.next_client_id
-                }
-            self.connected_clients.append(new_client)
-            self.next_client_id += 1
-            if broadcast_addition:
-                self.broadcast(client_sock, 
-                                "{0} has joined {1}".format(client_sock.getpeername(), 
-                                self.name))
-            return new_client
+            existing_client = find_client_by_name(client_nickname)
+            if not existing_client:
+                new_client = {
+                    "nickname": client_nickname, 
+                    "sock": client_sock,
+                    "join_id": self.next_client_id,
+                    "status": "online"
+                    }
+                self.connected_clients.append(new_client)
+                self.next_client_id += 1
+                if broadcast_addition:
+                    self.broadcast(client_sock, 
+                                    "{0} has joined {1}".format(client_sock.getpeername(), 
+                                    self.name))
+                return new_client
+            else:
+                existing_client["status"] = "online"
+                #broacast maybe
+                return existing_client
+
         except Exception as e:
             print(e)
             return None
@@ -35,7 +54,7 @@ class ChatRoom(object):
     def remove_client(self, client_nickname):
         for client in self.connected_clients:
             if client["nickname"] == client_nickname:
-                self.connected_clients.remove(client)
+                client["status"] == "offline"
                 leave_message = "CHAT: {0}\nCLIENT_NAME: {1}\nMESSAGE: {1} has left this chatroom\n\n".format(
                     self.id,
                     client["nickname"],
@@ -45,7 +64,7 @@ class ChatRoom(object):
 
     def broadcast(self, sender, message):
         for client in self.connected_clients:
-            if client["sock"] is not self.server_sock:
+            if client["sock"] is not self.server_sock and client["status"] == "online":
                 try:
                     client["sock"].sendall(message.encode())
                 except Exception as e:
