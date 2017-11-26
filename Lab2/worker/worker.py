6bd98d37d3fc0b68f4c4519d2cf58b5fb403b1aa
Conor Maguire <conormag94@gmail.com>
2017-11-26 17:40:09 +0000

    Add cloning of repo using pygit2

--- Lab2/worker/worker.py
+++ Lab2/worker/worker.py
@@ -1,5 +1,23 @@
+import requests
+from pygit2 import Repository, clone_repository, GitError
+
 from radon.complexity import cc_visit
 
+REPO_PATH = './repo'
+REPO_URL = 'https://github.com/conormag94/pyscores.git'
 
 
+def get_repo_obj():
+    try:
+        repo = Repository(REPO_PATH + '/.git')
+    except GitError as e:
+        repo = clone_repository(REPO_URL, REPO_PATH)
+    return repo
+
+def main():
+    repo = get_repo_obj()
+    print(repo)
+    
+if __name__ == '__main__':
+    main()
 
